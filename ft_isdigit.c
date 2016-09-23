bbbc175cc743df5f4e3e2bad3e4ad01ee88c8fea
Can Yildirim <cyildiri@e1z1r4p24.42.us.org>
2016-09-23 00:41:14 -0700

    removed test code

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,9 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/22 23:59:41 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/23 00:20:19 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/23 00:40:22 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int ft_isdigit(int c)
 {
@@ -21,20 +19,3 @@ int ft_isdigit(int c)
 	return (0);
 }
 
-int main()
-{
-	int i;
-
-	i = 0;
-	i = ft_isdigit('9');
-	printf("%d", i);
-	i = ft_isdigit('5');
-	printf("%d", i);
-	i = ft_isdigit('0');
-	printf("%d", i);
-	i = ft_isdigit(' ');
-	printf("%d", i);
-	i = ft_isdigit('z');
-	printf("%d", i);
-	return (0);
-}
