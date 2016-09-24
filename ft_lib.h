55ced2825cb73c3e7e39e9a53427cb2f0f22e686
Can Yildirim <cyildiri@e1z1r4p29.42.us.org>
2016-09-24 15:15:58 -0700

    ft_isprint done tested normed

--- ft_lib.h
+++ ft_lib.h
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 20:14:04 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/23 20:56:02 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/24 14:39:23 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
+int ft_isprint(int c);
 
 #endif
