4d81a27c1539b268d70bab20b861bf89f7570ee1
Can Yildirim <cyildiri@e1z1r4p17.42.us.org>
2016-09-25 10:50:46 -0700

    ft_strcpy untested

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lib.h                                           :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/23 20:14:04 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/25 10:34:32 by cyildiri         ###   ########.fr       */
+/*   Created: 2016/09/25 10:31:26 by cyildiri          #+#    #+#             */
+/*   Updated: 2016/09/25 10:48:21 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIB_H
-# define FT_LIB_H
+char    *strcpy(char *dst, const char *src)
+{
+	int length;
+	int index;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_strlen(char* str);
-int		ft_strdup(const char *s1);
-void    ft_bzero(void *s, size_t n);
-char	*strcpy(char *dst, const char *src);
-
-#endif
+	length = ft_strlen(src);
+	dst = (char *)malloc(sizeof(char) * (length + 1));
+	dst[length] = '\0';
+	index = 0;
+	while (src[index] != '\0')
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	return (dst);
+}
