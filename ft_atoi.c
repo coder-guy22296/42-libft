b6f624477badc6736dbcaca972b8cfaf1598ffdc
Can Yildirim <cyildiri@e1z1r4p17.42.us.org>
2016-09-25 16:59:51 -0700

    ft_atoi incomplete(edge cases) untested

---
+++ ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 16:18:31 by cyildiri          #+#    #+#             */
+/*   Updated: 2016/09/25 16:58:47 by cyildiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(const char *str)///NO ERROR condtions handled test + fix req!
+{
+	int	num;
+	int	len;
+	int	i;
+	int	multiplier;
+
+	i = 0;
+	num = 0;
+	multiplier = 1;
+	while (str[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		num += multiplier * (str[i] - '0');
+		multiplier *= 10
+		i--;
+	}
+	return (num);
+}
