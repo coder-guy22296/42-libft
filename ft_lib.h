da6738ca0d7ad77dd7fcf0a4151eaf54bed95353
Can Yildirim <cyildiri@Bocals-iMac-61.local>
2016-09-25 21:03:44 -0700

    ft_memmove untested

--- ft_lib.h
+++ ft_lib.h
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 20:14:04 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/25 20:07:01 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/25 21:03:08 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 void    *ft_memchr(const void *s, int c, size_t n);
 int 	ft_memcmp(const void *s1, const void *s2, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 20:18:59 by cyildiri          #+#    #+#             */
+/*   Updated: 2016/09/25 21:03:10 by cyildiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	int 			index;
+	unsigned char 	*src_ptr;
+    unsigned char   *dst_ptr;
+	unsigned char	*new_dst;
+	unsigned char	*old_data;
+	
+	index = 0;
+	src_ptr = (unsigned char *)src;
+    dst_ptr = (unsigned char *)dst;
+	new_dst  = (unsigned char *)malloc(len);
+	old_data = (unsigned char *)malloc(len);
+	while (index < len)
+	{
+		new_dst[index] = src_ptr[index];
+		old_data[index] = dst_ptr[index];
+		index++;
+	}
+	dst = new_dst;
+	return (old_data);
+}
