184b80c7ead25c6fb456bd8241560d87c7d0fe67
Can Yildirim <cyildiri@e1z1r4p14.42.us.org>
2016-09-27 16:06:47 -0700

    ft_strcpy fix should not malloc

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,26 +6,22 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 10:31:26 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/26 12:42:12 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/27 16:05:59 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "ft_lib.h"
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	int length;
 	int index;
 
-	length = ft_strlen(src);
-	dst = (char *)malloc(sizeof(char) * (length + 1));
-	dst[length] = '\0';
 	index = 0;
 	while (src[index] != '\0')
 	{
 		dst[index] = src[index];
 		index++;
 	}
+	dst[index] = '\0';
 	return (dst);
 }
