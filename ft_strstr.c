2e83f2c19ca3f4acdf219d1f31c24fe5c39cfb17
Can Yildirim <cyildiri@e1z1r4p14.42.us.org>
2016-09-28 14:17:00 -0700

    ft_str str normed tested

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,32 +6,29 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 13:44:46 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/26 12:12:06 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/28 13:22:21 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "ft_lib.h"
 
 char	*ft_strstr(const char *big, const char *little)
 {
 	char	*ptr;
 	int		index;
-	int		i;
 
 	index = 0;
-	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
 	while (big[index] != '\0')
 	{
-		if (big[index] == little[i] && little[i] != '\0')
+		if (big[index] == little[0])
 		{
-			if (i == 0)
-				ptr = (char *)&big[index];
-			else if (little[i + 1] == '\0')
+			ptr = (char *)&big[index];
+			if (!ft_memcmp(ptr, little, ft_strlen(little) - 1))
 				return (ptr);
-			i++;
 		}
-		else
-			i = 0;
 		index++;
 	}
 	return (NULL);
