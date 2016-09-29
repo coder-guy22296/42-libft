b40aa9d0328af7784a921836fd8f89e0f4c11110
Can Yildirim <cyildiri@e1z1r4p14.42.us.org>
2016-09-28 17:15:23 -0700

    memcmp fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 19:43:55 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/26 13:52:35 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/28 16:14:49 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
 
+	if (n == 0)
+		return (0);
 	index = 0;
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
-	while (index < n && ptr1[index] == ptr2[index])
+	while (index < n && ptr1[index] == ptr2[index]){
+		if (index == n - 1)
+			return (0);
 		index++;
-	if (n == 0)
-		return (0);
-	else if (ptr1[index] != ptr2[index])
+	}
+	if (ptr1[index] != ptr2[index])
 		return ((int)ptr1[index] - (int)ptr2[index]);
 	else
 		return (0);
