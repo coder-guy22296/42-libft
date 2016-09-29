b7d71fdae562de25ab87ea2580b1a4dacaed3da6
Can Yildirim <cyildiri@e1z1r5p15.42.us.org>
2016-09-28 21:06:43 -0700

    ft_strnstr now behaves as the documentation intends

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 14:15:39 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/09/26 14:23:02 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/09/28 20:25:42 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	char	*ptr;
 	int		index;
+	int		lil_len;
 
 	index = 0;
+	lil_len = ft_strlen(little);
 	if (*little == '\0')
 		return ((char *)big);
-	while (index < (int)len)
+	while (index < (int)len && big[index] != '\0')
 	{
 		if (big[index] == little[0])
 		{
 			ptr = (char *)&big[index];
-			if (ft_strlen(little) <= (int)len - index)
-				if (!ft_memcmp(ptr, little, ft_strlen(little)))
-					return (ptr);
+			if (lil_len <= (int)len - index)
+				if (lil_len <= ft_strlen(big) - index)
+					if (!ft_memcmp(ptr, little, ft_strlen(little)))
+						return (ptr);
 		}
 		index++;
 	}
