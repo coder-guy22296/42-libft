8d98a0022d0481d57a7e895852e809ea0e31a248
Can Yildirim <cyildiri@e1z1r6p22.42.us.org>
2016-10-03 13:15:09 -0700

    ft_memalloc fix protecting malloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/02 21:28:59 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/10/03 10:31:59 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/10/03 13:13:29 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memalloc(size_t size)
 	char	*mem;
 	int		i;
 
-	if (mem = (char *)malloc(size))
+	if (!(mem = (char *)malloc(size)))
 		return (NULL);
 	i = 0;
 	while (i < size)
