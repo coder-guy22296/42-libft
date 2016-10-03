e0da79e01841e0c7be1a940cc7a1deb72db607f1
Can Yildirim <cyildiri@e1z1r6p22.42.us.org>
2016-10-03 16:16:15 -0700

    ft_strmap untested

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cyildiri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 20:14:04 by cyildiri          #+#    #+#             */
-/*   Updated: 2016/10/03 14:41:32 by cyildiri         ###   ########.fr       */
+/*   Updated: 2016/10/03 16:15:45 by cyildiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void    ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
 
 #endif
